b81761c003eb3a8c9b97a0f633ee3ad19e954fc9
fritzgt <tuanis@live.com>
2020-10-15 13:37:54 +0300

    completed challenge

--- rotate_image.py
+++ rotate_image.py
@@ -6,12 +6,28 @@ class Solution:
         col = row
 
         # Transpose the matrix
-
+        for i in range(row):
+            for j in range(i, col):  # [[1, 2], [4, 3]]
+                temp = matrix[i][j]  # 1
+                matrix[i][j] = matrix[j][i]  # 1 = 4 now [[4, 2], [4, 3]]
+                matrix[j][i] = temp  # 3 = 2 now [[4, 2], [1, 4]]
+                #Result = [[3, 2], [1, 4]]
         # Swap the values of each row
+        for i in range(row):
+            for j in range(int(col/2)):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[i][col-1-j]
+                matrix[i][col-1-j] = temp
+            
 
 
-image = [[1, 2], [3, 4]]
+#[4, 1]
+#[3, 2]
+image = [[1, 2], [4, 3]]
+
 
 solution = Solution()
 
-solution.rotate(image)
+c = solution.rotate(image)
+
+print(image)
