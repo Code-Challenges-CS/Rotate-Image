400801dd02321664354040816c771b6f0bed7892
fritzgt <tuanis@live.com>
2020-10-15 13:57:11 +0300

    added explenation

--- rotate_image.py
+++ rotate_image.py
@@ -9,25 +9,26 @@ class Solution:
         for i in range(row):
             for j in range(i, col):  # [[1, 2], [4, 3]]
                 temp = matrix[i][j]  # 1
-                matrix[i][j] = matrix[j][i]  # 1 = 4 now [[4, 2], [4, 3]]
-                matrix[j][i] = temp  # 3 = 2 now [[4, 2], [1, 4]]
-                #Result = [[3, 2], [1, 4]]
+                matrix[i][j] = matrix[j][i]  # 1 = 4 now [[1, 4], [4, 3]]
+                matrix[j][i] = temp  # 3 = 2 now [[1, 4], [2, 3]]
+                #Result = [[1, 4], [2, 3]]
+
         # Swap the values of each row
         for i in range(row):
-            for j in range(int(col/2)):
-                temp = matrix[i][j]
-                matrix[i][j] = matrix[i][col-1-j]
-                matrix[i][col-1-j] = temp
-            
+            for j in range(int(col/2)):  # Devide to swap once
+                temp = matrix[i][j]  # 4 -> [[1, 4], [2, 3]]
+                matrix[i][j] = matrix[i][col-1-j]  # [[1, 1], [2, 3]]
+                matrix[i][col-1-j] = temp  # [[4, 1], [2, 3]]
+                #Result = [[4, 1], [3, 2]]
+
+# Process
+# [4, 1] -> [1, 4] -> [4, 1]
+# [3, 2] -> [2, 3] -> [3, 2]
 
 
-#[4, 1]
-#[3, 2]
 image = [[1, 2], [4, 3]]
 
 
 solution = Solution()
 
-c = solution.rotate(image)
-
-print(image)
+solution.rotate(image)
